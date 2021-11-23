69481db44e7ea89c46ef8defca7fdb0b5d293365
myungii <myung_gii@naver.com>
2021-11-24 02:58:10 +0900

    test

--- ci/app/Controllers/Home.php
+++ ci/app/Controllers/Home.php
@@ -2,10 +2,25 @@
 
 namespace App\Controllers;
 
+ //class Home extends BaseController
+ //{
+ //    public function index()
+ //    {
+ //        return view('welcome_message');
+ //    }
+ //}
+
+
 class Home extends BaseController
 {
     public function index()
     {
-        return view('welcome_message');
+        return view('test');
     }
 }
+
+
+
+
+
+
---
+++ ci/app/Views/test.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>test test!!!</title>
+	<meta name="description" content="The small framework with powerful features">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="shortcut icon" type="image/png" href="/favicon.ico"/>
+
+</head>
+
+<body>
+
+	<?php echo("잘되는 겁니다!"); ?>	
+
+</body>
+</html>
